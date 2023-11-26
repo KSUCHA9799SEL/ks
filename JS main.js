7d2bfd5f3a8991bc54225ce15d8691d02dd19466
Ksenia <kdzyuina@yandex.ru>
2023-11-26 20:47:13 +0400

    first commit

--- JS main.js
+++ JS main.js
@@ -3,8 +3,8 @@ const dropdownMenu = document.getElementById('dropdownMenu')
 
 console. log(toggleMenuBtn)
 toggleMenuBtn.addEventListener('click', function () {
-    if (dropdownMenu.style.disply === 'none' || dropdownMenu.style.disply === "") {
-        dropdownMenu.style.disply = 'block'
+    if (dropdownMenu.style.display === 'none' || dropdownMenu.style.disply === "") {
+        dropdownMenu.style.display = 'block'
     } else {
         dropdownMenu.style.disply = 'none'
     }
@@ -30,6 +30,45 @@ function showSlide(index) {
     })
 }
 showSlide(currentSlide)
-prevBtn.addEventListener('click', function () {
-    currentSlide = (currentSlide - 1)
+
+prevBtn.addEventListener('click', function (){
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length
+    showSlide(currentSlide)
+})
+nextBtn.addEventListener('click', function (){
+    currentSlide = (currentSlide + 1) % slides.length
+    showSlide(currentSlide)
+})
+
+document.getElementById('feedbackForm').addEventListener('submit', function (event) {
+     event.preventDefault();
+    let formData = new FormData(this);
+   
+    let feedbackData = {};
+    
+    FormData.forEach(function(value, key) {
+        feedbackData[key] = value;
+    });
+    
+    feedbackData['userId'] = 1;
+    
+    fetch('https://jsonplaceholder.typicode.com/posts ', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(feedbackData),
+    })
+    .then(response => response.json())
+    .then(data => {
+        
+        console.log('Успешно отправлено:', data);
+        
+        then.resset();
+    })
+    .catch(error => {
+       
+        console.error('Ошибка:', error);
+        
+    }) 
 })
